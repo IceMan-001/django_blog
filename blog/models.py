40466c74118be4e4b117adb57cd575b718fe637a
Andrey Bobrov <8867006@mail.ru>
2024-07-21 13:49:36 +0300

    Second commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    # поля таблицы
+    author = models.CharField(max_length=50, verbose_name='Автор')
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст поста')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Опубликовано',
+                                      editable=False)  # Параметр 'editable' отключает возможность для редактирования
+
+    class Meta:  # Настроечный класс
+        # Имя таблицы в единственном числе
+        verbose_name = 'Пост'
+        # Имя таблицы
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from .forms import PostForm
+from .models import Post
+
 
 def root(request):
     return render(request, template_name='blog/index.html')
@@ -23,3 +26,34 @@ def about(request):
 def send_data(request):
     data = {'name': 'Elena', 'age': 36}
     return JsonResponse(data=data)
+
+
+def add_post(request):
+    if request.method == 'GET':
+        form = PostForm()
+        context = {
+            'form': form,
+            'title': 'Добавление поста'
+        }
+        return render(request, template_name='blog/add_post.html', context=context)
+
+    if request.method == 'POST':
+        form = PostForm(data=request.POST)
+        if form.is_valid():
+            post = Post()
+            post.author = form.cleaned_data['author']
+            post.title = form.cleaned_data['title']
+            post.text = form.cleaned_data['text']
+            post.save()
+
+            return index(request)
+
+
+def post_list(request):
+    # Получаем все объекты модели Post
+    posts = Post.objects.all()
+    context = {
+        'title': 'Посты',
+        'posts': posts
+    }
+    return render(request, template_name='blog/posts.html', context=context)
