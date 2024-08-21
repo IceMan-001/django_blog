3ad7265dc7c17c7339ced151368cdfcd04c232cb
Andrey Bobrov <8867006@mail.ru>
2024-08-22 01:06:32 +0300

    Исправлена работа страницы 404

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1jov6dpqbvehpjjzz+&pv094jr0!sqsakkmjn68kd&r6^q=&a@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
